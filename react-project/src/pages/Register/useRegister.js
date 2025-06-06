71b458f07e968422c794f03d0aa666ca36e2fd5a
woojun <bjacau3@gmail.com>
2025-06-06 18:35:56 +0900

    ✨ 회원가입 기능 구현: Register.jsx, useRegister.js, registerApi.js
    - 회원가입 페이지 UI 구현 (ID, PW, 비밀번호 확인 입력창)
    - ID, PW, PW 확인 입력값 상태 관리 및 유효성 검사
    - 비밀번호 길이 조건 및 일치 여부 확인
    - 중복된 ID 확인 처리 (GET /user?loginId=xxx)
    - 신규 user, auth 데이터 json-server에 POST 저장
    - 회원가입 성공 시 /login 페이지로 이동

---
+++ react-project/src/pages/Register/useRegister.js
@@ -0,0 +1,53 @@
+// 회원가입 기능만 있는 파일입니다
+// 상태값 관리 + 버튼 클릭 처리만 관리
+
+import {useState} from "react"
+import {useNavigate} from "react-router-dom"
+import { getAuthByLoginId, createUserAndAuth } from "./registerApi";  
+import { getUserByLoginId } from "../Login/loginApi";
+
+export default function useRegister() {                     //id, 비번, 비번확인을 저장
+    const [loginId, setLoginId] = useState("");             //입력된 id저장
+    const [password, setPassword] = useState("");           //입력된 비번 저정
+    const [confirmPw, setConfirmPw] = useState("");         //비번 확인용 입력 저장
+    const [error, Error] = useState("");                    //에러 메세지 저장
+    const navigate = useNavigate();                         //페이지 이동
+
+    const handleRegister = async () => {                    //회원가입 버튼을 눌렀을때 
+        if (!loginId || !password || !confirmPw){           //필수 입력값 체크 ( 3개의 값들중 하나라도 비어있으면 true가 됨  )
+            setError("모든 항목을 입력해주세요");             //loginId가 비었거나, password가 비었거나, confirmPw가 비었으면
+            return;
+        }
+        
+        if (password.length < 8) {                          //비밀번호 길이 체크 
+            setError("비밀번호는 최소 8자 이상이어야 해요.");
+            return;
+        }
+
+        if (password !== confirmPw){                        //비밀번호와 확인값이 같은지 체크
+            setError("비밀번호가 일치하지 않습니다.");
+            return;
+        }
+
+        const existingUser = await getUserByLoginId(loginId);   //이미 존재하는 id인지 확인인
+        if (existingUser){
+            setError("이미 존재하는 ID 입니다.");
+            return;
+        }
+
+        try{
+            await createUserAndAuth(loginId, password);         //회원가입 요청(user + auth 테이블에 추가) 유저 + 비번 등록록
+            navigate("/login");                                 //완료되면 로그인 페이지로 이동 
+        } catch (e) {
+            setError("회원가입 중 오류 발생했어요.");
+        }
+    };
+
+    return{
+        loginId, setLoginId,
+        password, setPassword,
+        confirmPw, setConfirmPw,
+        error,
+        handleRegister,
+    };
+}
