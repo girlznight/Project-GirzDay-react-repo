c703f857025daae91b0a76034e9b66ce3045e719
YangYeJin <anxxx125@gamil.com>
2025-06-14 11:29:39 +0900

    📝 Post.jsx

--- react-project/src/components/AlertPopup.jsx
+++ react-project/src/components/AlertPopup.jsx
@@ -1,4 +1,3 @@
-// 전부 챗지피티
 import React from "react";
 import CustomButton from "./CustomButton";
 
@@ -6,21 +5,21 @@ function AlertPopup({ show, onYes, onNo, message }) {
   if (!show) return null;
 
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-[#fcfcf8] bg-opacity-60 flex justify-center items-center z-50">
-      <div className="bg-white p-6 rounded-xl w-80 text-center shadow-lg">
+    <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-20 flex justify-center items-center z-50">
+      <div className="bg-white/85 p-6 rounded-xl w-80 text-center shadow-lg">
         <p className="mb-6 text-lg font-medium">{message}</p>
         <div className="flex justify-center space-x-4">
-          <CustomButton
-            onClick={onYes}
-            className="w-24 h-10 border border-black text-black bg-transparent"
-          >
-            Yes
-          </CustomButton>
           <CustomButton
             onClick={onNo}
             className="w-24 h-10 bg-black text-white"
           >
             No
+          </CustomButton>
+          <CustomButton
+            onClick={onYes}
+            className="w-24 h-10 border border-black text-black bg-transparent"
+          >
+            Yes
           </CustomButton>
         </div>
       </div>
